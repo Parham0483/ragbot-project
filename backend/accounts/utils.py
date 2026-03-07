70003c2c7f22183082731a2f3cfc12cdbd66d902
Parham0483 <parham.g1383@gmail.com>
2026-03-07 17:25:31 +0000

    updated

--- backend/accounts/utils.py
+++ backend/accounts/utils.py
@@ -3,7 +3,7 @@ from chatbots.models import Message
 
 
 def get_monthly_usage(user):
-    """Count user-role messages across all of this user's chatbots in the current calendar month."""
+    """Count user-role messages across all of this user's chatbots """
     now = timezone.now()
     return Message.objects.filter(
         conversation__chatbot__owner=user,
